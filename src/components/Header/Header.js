23a9c324c23fd926d2598051605d177546b0142e
Kaustav Ghosh <kaustav@Kaustavs-MacBook-Air.local>
2022-09-06 02:50:41 +0530

    Updated navbar style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,73 +3,100 @@ import styled from 'styled-components'
 
 const StyledNav = styled.nav`
   background: #e8e6e6;
-  min-height: 50px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 `
 
-const StyledUl = styled.ul`
+const NavBar = styled.nav`
+  min-height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px;
+`
+
+const LeftUL = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
   display: flex;
+  flex: 1;
+  justify-content: flex-start;
+  gap: 10px;
 `
 
-const StyledLogo = styled.text`
-  cursor: pointer;
-  position: absolute;
-  top: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  max-width: 165px;
+const StyledLogo = styled.span`
+  flex: 1;
   text-align: center;
+  cursor: pointer;
   text-decoration: none;
   font-size: 16px;
   line-height: 19px;
   letter-spacing: 2px;
 `
 
+const RightUL = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  gap: 10px;
+`
+
 const StyledLi = styled.li`
   display: inline-block;
-  padding: 20px;
+  padding: 20px 10px;
   cursor: pointer;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 14px;
 
   &:hover,
   &:focus {
-    background-color: palevioletred;
+    color: #807e7d;
   }
   &:active {
-    color: red;
+    color: #1a0c02;
   }
 `
 
 const StyledSearchBar = styled.div`
-  border-top: 1px solid white;
-  width: 100%;
+  width: 100vw;
   height: 50px;
   background: #e8e6e6;
+`
+
+const Divider = styled.div`
+  height: 1px;
+  background-color: #969696;
+  margin-right: 20px;
+  margin-left: 20px;
+
 `
 
 function Header() {
   const [ searchBarOpen, setSearchBarOpen ] = useState(false)
 
-  return <>
-    <StyledNav>
-      <StyledUl>
+  const logoRedirect = () => console.log("Redirecting")
+
+  return <StyledNav>
+    <NavBar>
+      <LeftUL>
         <StyledLi>Featured</StyledLi>
         <StyledLi>For him</StyledLi>
         <StyledLi>For her</StyledLi>
-      </StyledUl>
-      <StyledLogo>TIDALCOUTURE</StyledLogo>
-      <StyledUl>
-        <StyledLi onClick={() => setSearchBarOpen(true)}>S</StyledLi>
+      </LeftUL>
+      <StyledLogo onClick={logoRedirect}>TIDALCOUTURE</StyledLogo>
+      <RightUL>
+        <StyledLi onClick={() => setSearchBarOpen(!searchBarOpen)}>S</StyledLi>
         <StyledLi>Sign in</StyledLi>
         <StyledLi>Cart</StyledLi>
-      </StyledUl>
-    </StyledNav>
-    {searchBarOpen && <StyledSearchBar onClick={() => setSearchBarOpen(false)}/>}
-  </>
+      </RightUL>
+    </NavBar>
+    {searchBarOpen && <>
+      <Divider />
+      <StyledSearchBar onClick={() => setSearchBarOpen(false)}/>
+    </>}
+  </StyledNav>
 }
 
 export default Header
